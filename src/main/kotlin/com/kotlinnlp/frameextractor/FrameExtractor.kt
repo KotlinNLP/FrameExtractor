65132fe5f99e1a726cadfd0db5f6bcabd0590ef9
KotlinNLP Authors <github@kotlinnlp.com>
2018-08-01 23:35:01 +0200

    Add FrameExtractor

--- src/main/kotlin/com/kotlinnlp/frameextractor/FrameExtractor.kt
+++ src/main/kotlin/com/kotlinnlp/frameextractor/FrameExtractor.kt
@@ -7,4 +7,30 @@
 
 package com.kotlinnlp.frameextractor
 
-class FrameExtractor
+import com.kotlinnlp.frameextractor.classifier.FrameClassifier
+import com.kotlinnlp.neuralparser.parsers.lhrparser.LatentSyntacticStructure
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+
+/**
+ * The frame extractor.
+ *
+ * @param classifier a frame classifier
+ */
+class FrameExtractor(private val classifier: FrameClassifier) {
+
+  /**
+   * Extract an intent frame from a given sentence that has been analyzed by a
+   * [com.kotlinnlp.neuralparser.parsers.lhrparser.LHRParser].
+   *
+   * @param lss a latent syntactic structure of a sentence
+   *
+   * @return the intent frame extracted
+   */
+  fun extractFrame(lss: LatentSyntacticStructure): Intent {
+
+    val tokenEncodings: List<DenseNDArray> = lss.contextVectors.zip(lss.latentHeads).map { it.first.concatH(it.second) }
+    val classifierOutput: FrameClassifier.Output = this.classifier.forward(tokenEncodings)
+
+    return classifierOutput.buildIntent(tokenForms = lss.sentence.tokens.map { it.form })
+  }
+}
