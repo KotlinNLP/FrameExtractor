1dde1495097b242abbc305a9541f0e4bab0e3fc8
KotlinNLP Authors <github@kotlinnlp.com>
2019-03-27 19:19:35 +0100

    Code refactoring

--- examples/training/TrainingUtils.kt
+++ examples/training/TrainingUtils.kt
@@ -1,9 +1,11 @@
-package training/* Copyright 2018-present The KotlinNLP Authors. All Rights Reserved.
+/* Copyright 2018-present The KotlinNLP Authors. All Rights Reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * ------------------------------------------------------------------*/
+
+package training
 
 import com.kotlinnlp.linguisticdescription.sentence.Sentence
 import com.kotlinnlp.linguisticdescription.sentence.token.FormToken
